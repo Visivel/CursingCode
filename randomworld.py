57656e15f541977f83c71d7903fe6a5c96adfbf3
Visivel <120154724+Visivel@users.noreply.github.com>
2023-05-12 15:41:44 -0400

    Create randomworld.py

---
+++ randomworld.py
@@ -0,0 +1,8 @@
+import random
+random = random.randint(1,1)
+if random == random:
+    Hello = "Hello"
+    world = "world"
+    print(Hello,world)
+else:
+    print("else if this dont work then: explode the fucking world")
